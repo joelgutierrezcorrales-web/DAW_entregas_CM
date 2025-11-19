6bb4da6cd4d3fcdcebd307448cb4f835447a690c
JoelGutierrez <joel.gutierrez.corrales@edu.thepower.education>
2025-11-19 13:21:55 +0100

    entrega

--- entrega_01/src/entrega_02/CalculadoraBasica.java
+++ entrega_01/src/entrega_02/CalculadoraBasica.java
@@ -8,19 +8,22 @@ public class CalculadoraBasica {
     public static void main(String[] args) throws dividir0Exception {
         System.out.println("=== CALCULADORA BÁSICA ===");
 
-        int a = sc.pedirNumero("Introduce el numero 1:");
+        double a = pedirNumero("Introduce el número 1:");
+        double b = pedirNumero("Introduce el número 2:");
 
-        if (a<0 || a>100) {
-            System.out.println("El numero introducido no es valido");
-            a = sc.pedirNumero("Introduce el numero 1:");
-        }
-
-        int b = sc.pedirNumero("Introduce el numero 2:");
-
-        if (b<0 || b>100) {
-            System.out.println("El numero introducido no es valido");
-            b = sc.pedirNumero("Introduce el numero 2:");
-        }
+//        int a = sc.pedirNumero("Introduce el numero 1:");
+//
+//        if (a<0 || a>100) {
+//            System.out.println("El numero introducido no es valido");
+//            a = sc.pedirNumero("Introduce el numero 1:");
+//        }
+//
+//        int b = sc.pedirNumero("Introduce el numero 2:");
+//
+//        if (b<0 || b>100) {
+//            System.out.println("El numero introducido no es valido");
+//            b = sc.pedirNumero("Introduce el numero 2:");
+//        }
 
         System.out.println("==== RESUMEN ====");
         System.out.println("Número 1:" +a);
@@ -33,6 +36,21 @@ public class CalculadoraBasica {
         } catch (dividir0Exception e) {
             System.out.println("División: "+e.getMessage());
         }
+    }
+
+    public static double pedirNumero(String mensaje){
+        double numero;
+
+
+        do {
+            System.out.print(mensaje);
+            numero = sc.pedirNumero("");
+
+            if (numero < 0 || numero > 100) {
+                System.out.println("Número fuera de rango. Intenta de nuevo.");
+            }
+        } while (numero < 0 || numero > 100);
+        return numero;
     }
 
     public static double sumar(double a, double b) {
