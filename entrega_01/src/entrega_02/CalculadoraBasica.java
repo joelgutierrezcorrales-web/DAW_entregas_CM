a6ed91f74996a7dbe7e7fcb36fd866e40bb1f978
JoelGutierrez <joel.gutierrez.corrales@edu.thepower.education>
2025-11-19 13:27:21 +0100

    entrega corregida, con comentarios añadidos

--- entrega_01/src/entrega_02/CalculadoraBasica.java
+++ entrega_01/src/entrega_02/CalculadoraBasica.java
@@ -8,29 +8,19 @@ public class CalculadoraBasica {
     public static void main(String[] args) throws dividir0Exception {
         System.out.println("=== CALCULADORA BÁSICA ===");
 
+        //Pedimos el numero con las dos variables que vamos a introducir
         double a = pedirNumero("Introduce el número 1:");
         double b = pedirNumero("Introduce el número 2:");
 
-//        int a = sc.pedirNumero("Introduce el numero 1:");
-//
-//        if (a<0 || a>100) {
-//            System.out.println("El numero introducido no es valido");
-//            a = sc.pedirNumero("Introduce el numero 1:");
-//        }
-//
-//        int b = sc.pedirNumero("Introduce el numero 2:");
-//
-//        if (b<0 || b>100) {
-//            System.out.println("El numero introducido no es valido");
-//            b = sc.pedirNumero("Introduce el numero 2:");
-//        }
-
+        //Escribirmnos lo que se va a mostrar por consola como resumen utilizando los metodos
         System.out.println("==== RESUMEN ====");
         System.out.println("Número 1:" +a);
         System.out.println("Número 2:" +b);
         System.out.println("Suma: " +sumar(a, b));
         System.out.println("Resta: " +restar(a, b));
         System.out.println("Multiplicacion: " +multiplicar(a, b));
+
+        //El metodo de division + la excepcion entre 0
         try{
             System.out.println("División: "+dividir(a, b));
         } catch (dividir0Exception e) {
@@ -38,6 +28,8 @@ public class CalculadoraBasica {
         }
     }
 
+
+    //Metodo pedir numero con bucle si los numeros no entrar dentro del rango
     public static double pedirNumero(String mensaje){
         double numero;
 
@@ -52,19 +44,25 @@ public class CalculadoraBasica {
         } while (numero < 0 || numero > 100);
         return numero;
     }
-
+    //Metodo sumar dos variables
     public static double sumar(double a, double b) {
         return a + b;
     }
 
+    //Metodo restar dos variables
     public static double restar(double a, double b) {
         return a - b;
     }
+
+    //Metodo multiplicar dos variables
     public static double multiplicar(double a, double b) {
         return a * b;
     }
+
+    //Metodo dividir dos variables
     public static double dividir(double a, double b) throws dividir0Exception {
 
+        //Si el numero 2 es 0 lanzamos la excepcion
         if (b == 0) {
             throw new dividir0Exception("No se puede dividir por 0");
         }
